a9731b1ac5a85a767b9b6c8f0dfa006a1c128616
Arthur MONTEIRO <atou.monteiro@gmail.com>
2019-07-24 11:07:17 +0200

    main try catch

--- Demo VK __1/main.cpp
+++ Demo VK __1/main.cpp
@@ -2,5 +2,16 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	try
+	{
+	}
+	catch (const std::runtime_error& e)
+	{
+		std::cerr << e.what() << std::endl;
+		system("PAUSE");
+		return EXIT_FAILURE;
+	}
+
+	system("PAUSE");
+	return EXIT_SUCCESS;
 }
